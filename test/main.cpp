ebe7fdc69a931fa54821f692f8e1a23768a7c6d1
Florian Weber <oss@florianjw.de>
2015-07-31 16:08:53 +0200

    make streams use the default ctor to support old, non-conforming stdlibs

--- test/main.cpp
+++ test/main.cpp
@@ -21,7 +21,7 @@ static inline bool head_and_tail_equal(const std::string& str, const std::string
 
 
 BOOST_AUTO_TEST_CASE(basic_log) {
-	auto stream = std::ostringstream{};
+	std::ostringstream stream{};
 	auto logger = logger::logger_set{stream};
 	logger.log(logger::level::note, "foo", " bar ", 23, ' ', 42.0, " baz");
 	BOOST_CHECK(head_and_tail_equal(stream.str(), "[note ][", "]: foo bar 23 42 baz\n"));
@@ -29,7 +29,7 @@ BOOST_AUTO_TEST_CASE(basic_log) {
 
 
 BOOST_AUTO_TEST_CASE(basic_logf) {
-	auto stream = std::ostringstream{};
+	std::ostringstream stream{};
 	auto logger = logger::logger_set{stream};
 	logger.logf(logger::level::note, "bla%sblub%s%%", "foo", 42);
 	BOOST_CHECK(head_and_tail_equal(stream.str(), "[note ][", "]: blafooblub42%\n"));
@@ -37,9 +37,9 @@ BOOST_AUTO_TEST_CASE(basic_logf) {
 
 
 BOOST_AUTO_TEST_CASE(log_hiding) {
-	auto stream1 = std::ostringstream{};
+	std::ostringstream stream1{};
 	auto logger1 = logger::logger_set{stream1};
-	auto stream2 = std::ostringstream{};
+	std::ostringstream stream2{};
 	auto logger2 = logger::logger_set{stream2};
 	logger::note("foobar");
 	BOOST_CHECK(stream1.str().empty());
@@ -48,10 +48,10 @@ BOOST_AUTO_TEST_CASE(log_hiding) {
 
 
 BOOST_AUTO_TEST_CASE(log_restoration) {
-	auto stream1 = std::ostringstream{};
+	std::ostringstream stream1{};
 	auto logger1 = logger::logger_set{stream1};
 	{
-		auto stream2 = std::ostringstream{};
+		std::ostringstream stream2{};
 		auto logger2 = logger::logger_set{stream2};
 	}
 	logger::note("foobar");
@@ -60,9 +60,9 @@ BOOST_AUTO_TEST_CASE(log_restoration) {
 
 
 BOOST_AUTO_TEST_CASE(non_global_log) {
-	auto stream1 = std::ostringstream{};
+	std::ostringstream stream1{};
 	auto logger1 = logger::logger_set{stream1};
-	auto stream2 = std::ostringstream{};
+	std::ostringstream stream2{};
 	auto logger2 = logger::logger_set{{stream2}, logger::auto_register::off};
 	logger::note("foobar");
 	BOOST_CHECK(head_and_tail_equal(stream1.str(), "[note ][", "]: foobar\n"));
@@ -72,31 +72,31 @@ BOOST_AUTO_TEST_CASE(non_global_log) {
 
 BOOST_AUTO_TEST_CASE(concat_alias_methods) {
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::debug}};
 		logger.debug("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[debug][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::note}};
 		logger.note("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[note ][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::warn}};
 		logger.warn("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[warn ][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::error}};
 		logger.error("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[error][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::fatal}};
 		logger.fatal("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[fatal][", "]: foo\n"));
@@ -106,31 +106,31 @@ BOOST_AUTO_TEST_CASE(concat_alias_methods) {
 
 BOOST_AUTO_TEST_CASE(format_alias_methods) {
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::debug}};
 		logger.debugf("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[debug][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::note}};
 		logger.notef("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[note ][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::warn}};
 		logger.warnf("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[warn ][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::error}};
 		logger.errorf("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[error][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::fatal}};
 		logger.fatalf("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[fatal][", "]: foo\n"));
@@ -140,31 +140,31 @@ BOOST_AUTO_TEST_CASE(format_alias_methods) {
 
 BOOST_AUTO_TEST_CASE(concat_alias_functions) {
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::debug}};
 		logger::debug("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[debug][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::note}};
 		logger::note("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[note ][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::warn}};
 		logger::warn("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[warn ][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::error}};
 		logger::error("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[error][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::fatal}};
 		logger::fatal("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[fatal][", "]: foo\n"));
@@ -174,31 +174,31 @@ BOOST_AUTO_TEST_CASE(concat_alias_functions) {
 
 BOOST_AUTO_TEST_CASE(format_alias_functions) {
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::debug}};
 		logger::debugf("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[debug][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::note}};
 		logger::notef("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[note ][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::warn}};
 		logger::warnf("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[warn ][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::error}};
 		logger::errorf("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[error][", "]: foo\n"));
 	}
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{{stream, logger::level::fatal}};
 		logger::fatalf("foo");
 		BOOST_CHECK(head_and_tail_equal(stream.str(), "[fatal][", "]: foo\n"));
@@ -207,7 +207,7 @@ BOOST_AUTO_TEST_CASE(format_alias_functions) {
 
 
 BOOST_AUTO_TEST_CASE(formatting_exceptions) {
-	auto stream = std::ostringstream{};
+	std::ostringstream stream{};
 	auto logger = logger::logger_set{stream};
 	BOOST_CHECK_THROW(logger.notef("%"), std::invalid_argument);
 	BOOST_CHECK_THROW(logger.notef("%s"), std::invalid_argument);
@@ -216,7 +216,7 @@ BOOST_AUTO_TEST_CASE(formatting_exceptions) {
 
 
 BOOST_AUTO_TEST_CASE(multiple_calls) {
-	auto stream = std::ostringstream{};
+	std::ostringstream stream{};
 	auto logger = logger::logger_set{stream};
 	logger::note("foo1");
 	logger::debug("foo2");
@@ -233,11 +233,11 @@ BOOST_AUTO_TEST_CASE(multiple_calls) {
 	BOOST_CHECK_EQUAL(foo2, std::string::npos);
 }
 BOOST_AUTO_TEST_CASE(multiple_calls_nested) {
-	auto stream = std::ostringstream{};
+	std::ostringstream stream{};
 	auto logger = logger::logger_set{stream};
 	logger::note("foo1");
 	{
-		auto stream = std::ostringstream{};
+		std::ostringstream stream{};
 		auto logger = logger::logger_set{stream};
 		logger::note("foo2");
 	}
@@ -253,9 +253,9 @@ BOOST_AUTO_TEST_CASE(multiple_calls_nested) {
 
 
 BOOST_AUTO_TEST_CASE(extending_current_logger) {
-	auto stream1 = std::ostringstream{};
+	std::ostringstream stream1{};
 	auto logger1 = logger::logger_set{stream1};
-	auto stream2 = std::ostringstream{};
+	std::ostringstream stream2{};
 	{
 		auto logger2 = logger::current_logger_extended({stream2});
 		logger::note("foo1");
